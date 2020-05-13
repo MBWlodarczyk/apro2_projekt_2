c2adc8a47d791731267c32f3b8b5bf1c199f462f
mwlodarc <m.wlodarczyk@stud.elka.pw.edu.pl>
2020-05-14 01:17:33 +0200

    minor fixes

--- core/src/Client/Model/Heroes/Warrior.java
+++ core/src/Client/Model/Heroes/Warrior.java
@@ -1,14 +1,16 @@
 package Client.Model.Heroes;
 
 import Client.Model.Player;
+import Client.Model.Skills.Walk;
 
 public class Warrior extends Hero{
 
         final int id;
 
         public Warrior(Player owner) {
-            super(owner, 110, 100, 3);
+            super(owner, 110, 100);
             id = idGen++;
+            this.skills.add(new Walk(3));
         }
 
 }
