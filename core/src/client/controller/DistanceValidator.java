2ba73215f7a6f41f3fe6c72dba30694531c664bd
mwlodarc <m.wlodarczyk@stud.elka.pw.edu.pl>
2020-05-16 17:17:19 +0200

    fix

--- core/src/client/controller/DistanceValidator.java
+++ core/src/client/controller/DistanceValidator.java
@@ -1,11 +1,10 @@
 package Client.Controller;
 
-import Client.Model.map.Field;
-import Client.Model.map.GameMap;
 import Client.Model.Heroes.Paladin;
 import Client.Model.Player;
 import Client.Model.Skills.Walk;
-import Client.Model.Type;
+import Client.Model.map.Field;
+import Client.Model.map.GameMap;
 
 /**
  * Class to validate if skill target is in range.
@@ -48,7 +47,7 @@ public class DistanceValidator {
      * Method to help dfs validate if the field is not null or wall.
      */
     private static boolean fieldValid(GameMap map, int y, int x) {
-        return y < map.getMap().length && y >= 0 && x >= 0 && x < map.getMap()[0].length && map.getMap()[y][x].getObstacle()==null;//Typy ktore nie moga byc przekroczone tutaj dodawac
+        return y < map.getMap().length && y >= 0 && x >= 0 && x < map.getMap()[0].length && map.getMap()[y][x].getObstacle().isCrossable();//Typy ktore nie moga byc przekroczone tutaj dodawac
     }
 
     /**
--- core/src/client/model/heroes/Warrior.java
+++ core/src/client/model/heroes/Warrior.java
@@ -8,7 +8,7 @@ public class Warrior extends Hero {
     final int id;
 
     public Warrior(Player owner) {
-        super(owner, 110, 100, 100);
+        super(owner, 110, 100, 100, 5);
         id = idGen++;
         this.skills.add(new Walk(3));
     }
